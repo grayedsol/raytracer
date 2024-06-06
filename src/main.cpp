c92d17caceab59fbf54f32df48a8fac3ebd7323f
Kevin Tu <kevin.xander.tu@gmail.com>
2024-06-06 16:37:18 -0500

    Edit lights

--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,13 @@ int main() {
 
     scene.spheres.push_back(Sphere{ Vec3f(2048, 500, 500), 400, GRY_Material{GRY_Color{50,50,255,255}} });
     scene.spheres.push_back(Sphere{ Vec3f(2048, 0, 0), 150, GRY_Material{GRY_Color{255,64,64,255}}  });
-    // scene.spheres.push_back(Sphere{ Vec3f(1500, 500, 500), 50, GRY_Material{GRY_Color{128,128,128,255}}  });
     scene.spheres.push_back(Sphere{ Vec3f(750, -200, -200), 50, GRY_Material{GRY_Color{26,100,26,255}}  });
     scene.spheres.push_back(Sphere{ Vec3f(1000, 250, -400), 100, GRY_Material{GRY_Color{230,230,100,255}} });
     scene.spheres.push_back(Sphere{ Vec3f(1000, 225, -250), 100, GRY_Material{GRY_Color{26,26,128,255}} });
     scene.spheres.push_back(Sphere{ Vec3f(1000, -400, 300), 250, GRY_Material{GRY_Color{100,64,128,255}} });
 
-    scene.lights.push_back(Light{ Vec3f(500, 750, -750), 0.5f });
-    scene.lights.push_back(Light{ Vec3f(500, 0, 1000), 0.5f });
-    scene.lights.push_back(Light{ Vec3f(500, 0, -1000), 0.5f });
+    scene.lights.push_back(Light{ Vec3f(500, 750, -750), 0.75f });
+    scene.lights.push_back(Light{ Vec3f(500, 0, 1000), 0.75f });
 
     ppm.drawGradient();
 
