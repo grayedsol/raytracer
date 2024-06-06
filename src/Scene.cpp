4894330c53df141f4ee7fe5fbe7e2f2e65af135d
Kevin Tu <kevin.xander.tu@gmail.com>
2024-06-06 16:35:12 -0500

    Change variable name

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -1,13 +1,13 @@
 #include "Scene.hpp"
 
 void Scene::drawScene(const Vec3f& origin, const std::vector<Vec3f>& rays, GRY_Ppm& ppm) {
-    GRY_Color material;
+    GRY_Color color;
     Vec3f point;
     for (int i = 0; i < ppm.height; i++) {
         for (int j = 0; j < ppm.width; j++) {
-            if (rayIntersect(spheres, lights, origin, rays.at(i*ppm.width+j), point, material)) {
-                ppm.frameBuffer.at(i*ppm.width+j) = material;
+            if (rayIntersect(spheres, lights, origin, rays.at(i*ppm.width+j), point, color)) {
+                ppm.frameBuffer.at(i*ppm.width+j) = color;
             }
         }
-    }    
+    }
 }
