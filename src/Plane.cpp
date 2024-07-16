303f1ad6c674bbb63499a974eb3baa8f29b39e65
Kevin Tu <kevin.xander.tu@gmail.com>
2024-07-15 20:25:01 -0500

    Edit formula to use n.r = d instead of n.r = -d

--- src/Plane.cpp
+++ src/Plane.cpp
@@ -3,7 +3,7 @@
 bool rayIntersectPlane(const Plane &plane, const Vec3f &nRay, const Vec3f &rayOrigin, float *distance) {
     float d = GRY_VecDot(plane.n, nRay);
     if (d == 0.0f) { return false; }
-    float t = -((GRY_VecDot(plane.n, rayOrigin) + plane.d) / d);
+    float t = -((GRY_VecDot(plane.n, rayOrigin) - plane.d) / d);
     if (t <= 0.0f) { return false; }
     if (distance) { *distance = t; }
     return true;
