7243f8dcfa2ac80d440d17eb190a9734109230b4
Kevin Tu <kevin.xander.tu@gmail.com>
2024-07-15 22:44:25 -0500

    Simplify castRay and specifiy float constants

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -4,7 +4,7 @@
 
 void Scene::drawScene(const GRY_View& view, GRY_Ppm& ppm) {
     GRY_Color color;
-    GRY_Color ambient{0.0f, 0.0f, 0.0f, 1.0f};
+    GRY_Color ambient{0.f, 0.f, 0.f, 1.f};
     for (auto& light : lights) { ambient += (light.color * light.intensity) * 0.025f; }
     for (int i = 0; i < view.rays.size(); i++) {
         if (castRay(view.origin, view.rays.at(i), color)) {
@@ -14,14 +14,14 @@ void Scene::drawScene(const GRY_View& view, GRY_Ppm& ppm) {
 }
 
 bool Scene::castRay(const Vec3f &origin, const Vec3f &nRay, GRY_Color& color, int reflect) {
-    float leastDistanceToHit = -1.0f;
+    float leastDistanceToHit = -1.f;
     const Sphere* hitSphere = nullptr;
     const Plane* hitPlane = nullptr;
 
     for (auto& sphere : spheres) {
         float distance;
         if (rayIntersectSphere(sphere, nRay, origin, &distance) && 
-            (distance < leastDistanceToHit || leastDistanceToHit < 0.0f && distance >= 0.0f)) {
+            (distance < leastDistanceToHit || (leastDistanceToHit < 0.f && distance >= 0.f))) {
             leastDistanceToHit = distance;
             hitSphere = &sphere;
         }
@@ -29,7 +29,7 @@ bool Scene::castRay(const Vec3f &origin, const Vec3f &nRay, GRY_Color& color, in
     for (auto& plane : planes) {
         float distance;
         if (rayIntersectPlane(plane, nRay, origin, &distance) &&
-            (distance < leastDistanceToHit || leastDistanceToHit < 0.0f && distance >= 0.0f)) {
+            (distance < leastDistanceToHit || (leastDistanceToHit < 0.f && distance >= 0.f))) {
                 hitSphere = nullptr;
                 leastDistanceToHit = distance;
                 hitPlane = &plane;
@@ -40,31 +40,25 @@ bool Scene::castRay(const Vec3f &origin, const Vec3f &nRay, GRY_Color& color, in
         Vec3f point = origin + (nRay * leastDistanceToHit);
         Vec3f N = GRY_VecNormalize(point - hitSphere->center);
         GRY_Material mat = hitSphere->material;
-        if (hitSphere->material.refract && hitSphere->material.reflect && reflect < reflectionLimit) {
-            Vec3f refractRay = GRY_VecRefract(nRay, N, 1.0f, hitSphere->material.refractIndex);
-            Vec3f reflectRay = GRY_VecReflect(refractRay, N) * -1.0f;
-            Vec3f reflectOrigin = GRY_VecDot(reflectRay, N) < 0 ? point - (N * 0.001f) : point + (N * 0.001f);
 
-            if (castRay(reflectOrigin, reflectRay, color, reflect+1)) { mat.diffuseColor = color; }
-        }
-        else if (hitSphere->material.refract && reflect < reflectionLimit) {
-            Vec3f refractRay = GRY_VecRefract(nRay, N, 1.0f, hitSphere->material.refractIndex);
-            Vec3f refractOrigin = GRY_VecDot(refractRay, N) < 0 ? point - (N * 0.001f) : point + (N * 0.001f);
-
-            if (castRay(refractOrigin, refractRay, color, reflect+1)) { mat.diffuseColor = color; }
-        }
-        else if (hitSphere->material.reflect && reflect < reflectionLimit) {
-            Vec3f reflectRay = GRY_VecReflect(nRay, N);
-            Vec3f reflectOrigin = GRY_VecDot(reflectRay, N) < 0 ? point - (N * 0.0001f) : point + (N * 0.0001f);
-
-            if (castRay(reflectOrigin, reflectRay, color, reflect+1)) { mat.diffuseColor = color; }
+        Vec3f eRay = nRay;
+        if (reflect < reflectionLimit && (hitSphere->material.reflect || hitSphere->material.refract)) {
+            if (hitSphere->material.refract) {
+                eRay = GRY_VecRefract(eRay, N, 1.f, hitSphere->material.refractIndex);
+                if (hitSphere->material.reflect) { eRay = eRay * -1.f; }
+            }
+            if (hitSphere->material.reflect) {
+                eRay = GRY_VecReflect(eRay, N);
+            }
+            Vec3f eOrigin = GRY_VecDot(eRay, N) < 0.f ? point - (N * 1e-3f) : point + (N * 1e-3f);
+            if (castRay(eOrigin, eRay, color, reflect+1)) { mat.diffuseColor = color; }
         }
         color = blinnPhong(this, mat, N, point, origin, nRay);
     }
 
     else if (hitPlane) {
         Vec3f point = origin + (nRay * leastDistanceToHit);
-        Vec3f N = hitPlane->n * (GRY_VecDot(nRay, hitPlane->n) > 0 ? -1.0f : 1.0f);
+        Vec3f N = hitPlane->n * (GRY_VecDot(nRay, hitPlane->n) > 0.f ? -1.f : 1.f);
         GRY_Material mat = hitPlane->material;
         color = blinnPhong(this, mat, N, point, origin, nRay);
     }
@@ -76,13 +70,13 @@ bool Scene::castRay(const Vec3f& origin, const Vec3f& nRay, const float* withinD
     float distance;
     for (auto& sphere : spheres) {
         if (rayIntersectSphere(sphere, nRay, origin, &distance)) {
-            if (withinDistance && distance > *withinDistance) { return false; }
+            if (withinDistance && distance > *withinDistance) { continue; }
             return true;
         }
     }
     for (auto& plane : planes) {
         if (rayIntersectPlane(plane, nRay, origin, &distance)) {
-            if (withinDistance && distance > *withinDistance) { return false; }
+            if (withinDistance && distance > *withinDistance) { continue; }
             return true;
         }
     }
--- src/main.cpp
+++ src/main.cpp
@@ -11,25 +11,26 @@ int main() {
     GRY_Ppm ppm(width, height);
     GRY_View view(ppm, distanceFromScreen);
 
-    scene.spheres.push_back(Sphere{ Vec3f(500, 100, 250), 125, GRY_Material{GRY_Color{.0f,.0f,1.0f,1.0f}, GRY_White, 21.0f, true, true, 2.0f} });
-    scene.spheres.push_back(Sphere{ Vec3f(2048, 0, 0), 150, GRY_Material{GRY_Color{1.0f,.25f,.25f,1.0f}}  });
-    scene.spheres.push_back(Sphere{ Vec3f(750, -200, -200), 50, GRY_Material{GRY_Color{.1f,.7f,.1f,1.0f}}  });
-    scene.spheres.push_back(Sphere{ Vec3f(1000, 250, -400), 100, GRY_Material{GRY_Color{.9f,.9f,.325f,1.0f}} });
-    scene.spheres.push_back(Sphere{ Vec3f(1000, 225, -250), 100, GRY_Material{GRY_Color{.1f,.1f,.5f,1.0f}} });
-    scene.spheres.push_back(Sphere{ Vec3f(1000, -400, 300), 250, GRY_Material{GRY_Color{.125f,.125f,.5f,1.0f}} });
-    scene.spheres.push_back(Sphere{ Vec3f(1500, 200, -800), 350, GRY_Material{GRY_Color{.8f,.2f,.5f,1.0f}, GRY_White, 15.0f, true}  });
-    scene.spheres.push_back(Sphere{ Vec3f(3000, 1250, 500), 500, GRY_Material{GRY_Color{1.0f,.5f,.0f,1.0f}, GRY_White, 15.0f} });
 
-    scene.planes.push_back(Plane{ Vec3f(0,-1,0), 1000, GRY_Material{GRY_Color{1.0f,1.0f,.0f,1.0f}} });
-    scene.planes.push_back(Plane{ Vec3f(0,1,0), 2500, GRY_Material{GRY_Color{1.0f,.25f,.25f,1.0f}} });
-    scene.planes.push_back(Plane{ Vec3f(-1,0,0), 5000, GRY_Material{GRY_Color{0.75f,.0f,.75f,1.0f}} });
-    scene.planes.push_back(Plane{ Vec3f(1,0,0), 5000, GRY_Material{GRY_Color{0.0f,1.0f,1.0f,1.0f}} });
-    scene.planes.push_back(Plane{ Vec3f(0,0,1), 2000, GRY_Material{GRY_Color{0.25f,.75f,.25f,1.0f}} });
-    scene.planes.push_back(Plane{ Vec3f(0,0,-1), 2000, GRY_Material{GRY_Color{0.25f,.25f,.75f,1.0f}} });
+    scene.spheres.push_back(Sphere{ Vec3f(2048, 0, 0), 150, GRY_Material{GRY_Color{1.f,.25f,.25f,1.f}}  });
+    scene.spheres.push_back(Sphere{ Vec3f(750, -200, -200), 50, GRY_Material{GRY_Color{.1f,.7f,.1f,1.f}}  });
+    scene.spheres.push_back(Sphere{ Vec3f(1000, 250, -400), 100, GRY_Material{GRY_Color{.9f,.9f,.325f,1.f}} });
+    scene.spheres.push_back(Sphere{ Vec3f(1000, 225, -250), 100, GRY_Material{GRY_Color{.1f,.1f,.5f,1.f}} });
+    scene.spheres.push_back(Sphere{ Vec3f(1000, -400, 300), 250, GRY_Material{GRY_Color{.125f,.125f,.5f,1.f}} });
+    scene.spheres.push_back(Sphere{ Vec3f(1500, 200, -800), 350, GRY_Material{GRY_Color{.8f,.2f,.5f,1.f}, GRY_White, 15.f, true}  });
+    scene.spheres.push_back(Sphere{ Vec3f(3000, 1250, 500), 500, GRY_Material{GRY_Color{1.f,.5f,0.f,1.f}, GRY_White, 15.f} });
+    scene.spheres.push_back(Sphere{ Vec3f(100, 0, 0), 40, GRY_Material{GRY_Color{0.f,0.f,1.f,1.f}, GRY_White, 21.f, false, true, 1.05f} });
+
+    scene.planes.push_back(Plane{ Vec3f(0,-1,0), 1000, GRY_Material{GRY_Color{1.f,1.f,0.f,1.f}} });
+    scene.planes.push_back(Plane{ Vec3f(0,1,0), 2500, GRY_Material{GRY_Color{1.f,.25f,.25f,1.f}} });
+    scene.planes.push_back(Plane{ Vec3f(-1,0,0), 5000, GRY_Material{GRY_Color{.75f,0.f,.75f,1.f}} });
+    scene.planes.push_back(Plane{ Vec3f(1,0,0), 5000, GRY_Material{GRY_Color{0.f,1.f,1.f,1.f}} });
+    scene.planes.push_back(Plane{ Vec3f(0,0,1), 2000, GRY_Material{GRY_Color{.25f,.75f,.25f,1.f}} });
+    scene.planes.push_back(Plane{ Vec3f(0,0,-1), 2000, GRY_Material{GRY_Color{.25f,.25f,.75f,1.f}} });
 
 
-    scene.lights.push_back(Light{ Vec3f(250, 750, -1500), 0.75f/* , GRY_Color{0.1f,0.3f,1.0f} */});
-    scene.lights.push_back(Light{ Vec3f(250, 450, 1500), 0.75f/* , GRY_Color{1.0f,0.3f,1.0f} */ });
+    scene.lights.push_back(Light{ Vec3f(0, 750, -1500), .75f /* , GRY_Color{0.1f,0.3f,1.f} */});
+    scene.lights.push_back(Light{ Vec3f(0, 450, 1500), .75f /* , GRY_Color{1.f,0.3f,1.f} */ });
 
     ppm.drawGradient();
 
