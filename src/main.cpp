d106bb5ef3686377a01ff8976ef1427438761707
Kevin Tu <kevin.xander.tu@gmail.com>
2024-07-16 22:08:11 -0500

    Edit objects

--- src/main.cpp
+++ src/main.cpp
@@ -11,24 +11,24 @@ int main() {
     GRY_Ppm ppm(width, height);
     GRY_View view(ppm, distanceFromScreen);
 
-    scene.spheres.push_back(Sphere{ Vec3f(2048, 0, 0), 150, GRY_Material{GRY_Color{1.f,.25f,.25f,1.f}}  });
+    scene.spheres.push_back(Sphere{ Vec3f(1024, 0, 0), 75, GRY_Material{GRY_Color{1.f,.25f,.25f,1.f}}  });
     scene.spheres.push_back(Sphere{ Vec3f(1200, -300, -300), 75, GRY_Material{GRY_Color{.1f,.7f,.1f,1.f}}  });
-    scene.spheres.push_back(Sphere{ Vec3f(1000, 250, -400), 100, GRY_Material{GRY_Color{.9f,.9f,.325f,1.f}} });
-    scene.spheres.push_back(Sphere{ Vec3f(1000, 225, -250), 100, GRY_Material{GRY_Color{.1f,.1f,.5f,1.f}} });
-    scene.spheres.push_back(Sphere{ Vec3f(900, -0, 200), 250, GRY_Material{GRY_Color{.125f,.125f,.5f,1.f}, GRY_White, 15.f, false, true, 2.3f} });
-    scene.spheres.push_back(Sphere{ Vec3f(1500, 200, -800), 350, GRY_Material{GRY_Color{.8f,.2f,.5f,1.f}, GRY_White, 15.f}  });
-    scene.spheres.push_back(Sphere{ Vec3f(3000, 1250, 500), 500, GRY_Material{GRY_Color{1.f,.5f,0.f,1.f}, GRY_White, 15.f} });
-    scene.spheres.push_back(Sphere{ Vec3f(1500, 300, 500), 300, GRY_Material{GRY_Color{0.f,0.f,1.f,1.f}, GRY_White, 21.f, false, false, 1.05f} });
+    scene.spheres.push_back(Sphere{ Vec3f(1500, 250, -400), 100, GRY_Material{GRY_Color{.9f,.9f,.325f,1.f}} });
+    scene.spheres.push_back(Sphere{ Vec3f(1500, 225, -250), 100, GRY_Material{GRY_Color{.1f,.1f,.5f,1.f}} });
+    scene.spheres.push_back(Sphere{ Vec3f(1200, -200, -100), 250, GRY_Material{GRY_Color{.125f,.125f,.5f,1.f}, GRY_White, 15.f, false, true, 1.02f} });
+    scene.spheres.push_back(Sphere{ Vec3f(2500, 200, 700), 450, GRY_Material{GRY_Color{.8f,.2f,.5f,1.f}, GRY_White, 15.f, true}  });
+    scene.spheres.push_back(Sphere{ Vec3f(1600, 400, 50), 150, GRY_Material{GRY_Color{1.f,.5f,0.f,1.f}, GRY_White, 15.f} });
+    scene.spheres.push_back(Sphere{ Vec3f(1500, 300, -130), 300, GRY_Material{GRY_Color{0.f,0.f,1.f,1.f}, GRY_White, 21.f, true, true, 1.02f} });
 
-    scene.planes.push_back(Plane{ Vec3f(0,0,-1), 2000, GRY_Material{GRY_Color{.25f,.25f,.75f,1.f}} });
-    scene.planes.push_back(Plane{ Vec3f(0,-1,0), 500, GRY_Material{GRY_Color{1.f,1.f,0.f,1.f}, GRY_White} });
-    scene.planes.push_back(Plane{ Vec3f(0,1,0), 2500, GRY_Material{GRY_Color{1.f,.25f,.25f,1.f}} });
-    scene.planes.push_back(Plane{ Vec3f(-1,0,0), 5000, GRY_Material{GRY_Color{.75f,0.f,.75f,1.f}} });
-    scene.planes.push_back(Plane{ Vec3f(1,0,0), 5000, GRY_Material{GRY_Color{.25f,.75f,.75f,1.f}, GRY_Color{.25f,.25f,.25f,1.f}} });
-    scene.planes.push_back(Plane{ Vec3f(0,0,1), 2000, GRY_Material{GRY_Color{.25f,.75f,.25f,1.f}} });
+    // scene.planes.push_back(Plane{ Vec3f(0,0,-1), 2000, GRY_Material{GRY_Color{.25f,.25f,.75f,1.f}} });
+    scene.planes.push_back(Plane{ GRY_VecNormalize(Vec3f(0,-1,0)), 500, GRY_Material{GRY_Color{1.f,1.f,0.f,1.f}, GRY_White, 15.f, true} });
+    scene.planes.push_back(Plane{ GRY_VecNormalize(Vec3f(0,1,0)), 5000, GRY_Material{GRY_Color{.4f,.8f,.8f,1.f}} });
+    // scene.planes.push_back(Plane{ Vec3f(-1,0,0), 5000, GRY_Material{GRY_Color{.75f,0.f,.75f,1.f}} });
+    // scene.planes.push_back(Plane{ Vec3f(1,0,0), 5000, GRY_Material{GRY_Color{.25f,.75f,.75f,1.f}, GRY_Color{.25f,.25f,.25f,1.f}} });
+    // scene.planes.push_back(Plane{ Vec3f(0,0,1), 2000, GRY_Material{GRY_Color{.25f,.75f,.25f,1.f}} });
 
-    scene.lights.push_back(Light{ Vec3f(50, 750, -1500), .75f /* , GRY_Color{0.1f,0.3f,1.f} */});
-    scene.lights.push_back(Light{ Vec3f(50, 450, 1500), .75f /* , GRY_Color{1.f,0.3f,1.f} */ });
+    scene.lights.push_back(Light{ Vec3f(50, 1500, -300), .75f /* , GRY_Color{0.1f,0.3f,1.f} */});
+    scene.lights.push_back(Light{ Vec3f(50, 750, 1500), .75f /* , GRY_Color{1.f,0.3f,1.f} */ });
 
     // scene.spheres.push_back(Sphere{ Vec3f(0, 0, 0), 500, GRY_Material{GRY_Color{1.f,.25f,.25f,1.f}, GRY_Black}  });
     // scene.spheres.push_back(Sphere{ Vec3f(400, 0, 0), 75, GRY_Material{GRY_Color{.25f,.25f,.25f,1.f}, GRY_White, 15.f, true}  });
