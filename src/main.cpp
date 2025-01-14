4d243af19f4b984f9baa818b5ed76cb24dbda40c
Kevin Tu <kevin.xander.tu@gmail.com>
2025-01-14 10:50:39 -0600

    Clean up comments

--- src/main.cpp
+++ src/main.cpp
@@ -20,21 +20,11 @@ int main() {
     scene.spheres.push_back(Sphere{ Vec3f(1600, 400, 50), 150, GRY_Material{GRY_Color{1.f,.5f,0.f,1.f}, GRY_White, 15.f} });
     scene.spheres.push_back(Sphere{ Vec3f(1500, 300, -130), 300, GRY_Material{GRY_Color{0.f,0.f,1.f,1.f}, GRY_White, 21.f, true, true, 1.02f} });
 
-    // scene.planes.push_back(Plane{ Vec3f(0,0,-1), 2000, GRY_Material{GRY_Color{.25f,.25f,.75f,1.f}} });
     scene.planes.push_back(Plane{ GRY_VecNormalize(Vec3f(0,-1,0)), 500, GRY_Material{GRY_Color{1.f,1.f,0.f,1.f}, GRY_White, 15.f, true} });
     scene.planes.push_back(Plane{ GRY_VecNormalize(Vec3f(0,1,0)), 5000, GRY_Material{GRY_Color{.4f,.8f,.8f,1.f}} });
-    // scene.planes.push_back(Plane{ Vec3f(-1,0,0), 5000, GRY_Material{GRY_Color{.75f,0.f,.75f,1.f}} });
-    // scene.planes.push_back(Plane{ Vec3f(1,0,0), 5000, GRY_Material{GRY_Color{.25f,.75f,.75f,1.f}, GRY_Color{.25f,.25f,.25f,1.f}} });
-    // scene.planes.push_back(Plane{ Vec3f(0,0,1), 2000, GRY_Material{GRY_Color{.25f,.75f,.25f,1.f}} });
 
-    scene.lights.push_back(Light{ Vec3f(50, 1500, -300), .75f /* , GRY_Color{0.1f,0.3f,1.f} */});
-    scene.lights.push_back(Light{ Vec3f(50, 750, 1500), .75f /* , GRY_Color{1.f,0.3f,1.f} */ });
-
-    // scene.spheres.push_back(Sphere{ Vec3f(0, 0, 0), 500, GRY_Material{GRY_Color{1.f,.25f,.25f,1.f}, GRY_Black}  });
-    // scene.spheres.push_back(Sphere{ Vec3f(400, 0, 0), 75, GRY_Material{GRY_Color{.25f,.25f,.25f,1.f}, GRY_White, 15.f, true}  });
-
-    // scene.lights.push_back(Light{ Vec3f(-100, -50, -250), .25f /* , GRY_Color{0.1f,0.3f,1.f} */});
-    // scene.lights.push_back(Light{ Vec3f(-100, 50, 250), .25f /* , GRY_Color{1.f,0.3f,1.f} */ });
+    scene.lights.push_back(Light{ Vec3f(50, 1500, -300), .75f });
+    scene.lights.push_back(Light{ Vec3f(50, 750, 1500), .75f });
 
     ppm.drawGradient();
 
